c4a5965bccaaa9470e9d4093e37118d590a08b77
droli <droli@LAPTOP>
2023-07-30 15:27:09 -0300

    Add project files.

---
+++ src/DevIO.ApplicationWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevIO.ApplicationWeb.MvcApplication" Language="C#" %>
---
+++ src/DevIO.ApplicationWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevIO.ApplicationWeb.Startup))]
+namespace DevIO.ApplicationWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
