aa4cc76072ce8c4dcfc9252d8860782727f55e2c
danilo-rodriuges38 <danilo.rodrigues38@gmail.com>
2023-07-30 17:03:37 -0300

    Iniciando o projetos de estudos com os itens iniciais preparados.

---
+++ src/DevIO.ApplicationMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevIO.ApplicationMVC.MvcApplication" Language="C#" %>
---
+++ src/DevIO.ApplicationMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevIO.ApplicationMVC.Startup))]
+namespace DevIO.ApplicationMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
