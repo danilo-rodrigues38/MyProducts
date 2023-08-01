4ef9597800cbefd731161ac30493e3e4b264bd3d
danilo-rodriuges38 <danilo.rodrigues38@gmail.com>
2023-08-01 19:05:21 -0300

    Interface de acesso a dados

---
+++ src/DevIO.Business/Models/Fornecedores/IEnderecoRepesitory.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Business.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Fornecedores
+{
+    public interface IEnderecoRepesitory : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor ( Guid FornecedorId );
+    }
+}
---
+++ src/DevIO.Business/Models/Produtos/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Produtos
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutoPorFornecedor ( Guid FornecedorId );
+        Task<IEnumerable<Produto>> ObterProdutoFornecedores ( );
+        Task<Produto> ObterProdutoFornecedores ( Guid id );
+    }
+}
