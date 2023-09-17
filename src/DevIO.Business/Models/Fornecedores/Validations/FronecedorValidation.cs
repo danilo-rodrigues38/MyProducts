f47aea3f5baa52ae94e5e7cc3d9761400b1cfa59
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-09-17 11:22:39 -0300

    Validando entidades com o FluentValidations

--- src/DevIO.Business/Models/Fornecedores/Validations/FronecedorValidation.cs
+++ src/DevIO.Business/Models/Fornecedores/Validations/FronecedorValidation.cs
@@ -1,20 +1,39 @@
-﻿using FluentValidation;
+﻿using DevIO.Business.Core.Validations.Documentos;
+using FluentValidation;
 
 namespace DevIO.Business.Models.Fornecedores.Validations
 {
-    internal class FronecedorValidation : AbstractValidator<Fornecedor>
+    public class FronecedorValidation : AbstractValidator<Fornecedor>
     {
         public FronecedorValidation()
         {
             // FuleFor - Cria as regras para o campo "Nome".
+            // PropertName - exibe na tela o nome do campo, neste caso "Nome".
+            // MinLength - valor mínimo permitido, no caso, 2 caracteres.
+            // MaxLength - valor máximo permitido, no caso, 200 caracteres.
             RuleFor ( f => f.Nome )
                 .NotEmpty ( )
-                // PropertName - exibe na tela o nome do campo, neste caso "Nome".
                 .WithMessage ( "O campo {PropertName} precisa ser fornecido!" )
                 .Length ( 2, 200 )
-                // MinLength - valor mínimo permitido, no caso, 2 caracteres.
-                // MaxLength - valor máximo permitido, no caso, 200 caracteres.
                 .WithMessage ( "O campo {PropertName} precisa ter entre {MinLength} e {MaxLength} caracteres." );
+
+            When ( f => f.TipoFornecedor == TipoFornecedor.PessoaFisica, ( ) =>
+            {
+                RuleFor ( f => f.Documento.Length ).Equal ( CpfValidacao.TamanhoCpf )
+                    .WithMessage ( "O campo Documento precesa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}." );
+
+                RuleFor ( f => CpfValidacao.Validar ( f.Documento ) ).Equal ( true )
+                    .WithMessage ( "O documento fornecido é inválido." );
+            } );
+
+            When ( f => f.TipoFornecedor == TipoFornecedor.PessoaJuridica, ( ) =>
+            {
+                RuleFor ( f => f.Documento.Length ).Equal ( CnpjValidacao.TamanhoCnpj )
+                    .WithMessage ( "O campo Documento precesa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}." );
+
+                RuleFor ( f => CnpjValidacao.Validar ( f.Documento ) ).Equal ( true )
+                    .WithMessage ( "O documento fornecido é inválido." );
+            } );
         }
     }
 }
---
+++ src/DevIO.Business/Models/Fornecedores/Validations/ProdutoValidation.cs
@@ -0,0 +1,25 @@
+﻿using DevIO.Business.Models.Produtos;
+using FluentValidation;
+
+namespace DevIO.Business.Models.Fornecedores.Validations
+{
+    public class ProdutoValidation : AbstractValidator<Produto>
+    {
+        public ProdutoValidation()
+        {
+            RuleFor ( c => c.Nome )
+                .NotEmpty ( ).WithMessage ( "O campo {PropertyName} precisa ser fornecido." )
+                .Length ( 2, 200 ).WithMessage ( "O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres." );
+
+            RuleFor ( c => c.Descricao )
+                .NotEmpty ( ).WithMessage ( "O campo {PropertyName} precisa ser fornecido." )
+                .Length ( 2, 1000 ).WithMessage ( "O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres." );
+
+            RuleFor ( c => c.Valor )
+                .NotEmpty ( ).WithMessage ( "O campo {PropertyName} precisa ser fornecido." )
+                // GreaterThan - onde o valor deve ser maior que 0 "Zero".
+                // GreaterThanOrEqualTo - onde o valor de ser maior ou igual a zero, dependendo da sua regra de negócios.
+                .GreaterThan (0).WithMessage ( "O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres." );
+        }
+    }
+}
