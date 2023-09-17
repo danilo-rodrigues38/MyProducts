59480fc5a24c4f12e736aae4f2728b28705b408f
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-09-17 09:55:57 -0300

    Validação com FluentValidation

---
+++ src/DevIO.Business/Models/Fornecedores/Validations/FronecedorValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace DevIO.Business.Models.Fornecedores.Validations
+{
+    internal class FronecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FronecedorValidation()
+        {
+            // FuleFor - Cria as regras para o campo "Nome".
+            RuleFor ( f => f.Nome )
+                .NotEmpty ( )
+                // PropertName - exibe na tela o nome do campo, neste caso "Nome".
+                .WithMessage ( "O campo {PropertName} precisa ser fornecido!" )
+                .Length ( 2, 200 )
+                // MinLength - valor mínimo permitido, no caso, 2 caracteres.
+                // MaxLength - valor máximo permitido, no caso, 200 caracteres.
+                .WithMessage ( "O campo {PropertName} precisa ter entre {MinLength} e {MaxLength} caracteres." );
+        }
+    }
+}
