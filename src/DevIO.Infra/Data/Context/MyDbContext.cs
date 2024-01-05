28ffa57d535e9fa38beb8f109c53123fe6f2183d
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2024-01-05 17:59:20 -0300

    Definindo data automática no Entity Framework

--- src/DevIO.Infra/Data/Context/MyDbContext.cs
+++ src/DevIO.Infra/Data/Context/MyDbContext.cs
@@ -1,8 +1,12 @@
-﻿using DevIO.Business.Models.Fornecedores;
-using DevIO.Business.Models.Produtos;
-using DevIO.Infra.Data.Mappings;
+﻿using System;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DevIO.Business.Models.Fornecedores;
+using DevIO.Business.Models.Produtos;
+using DevIO.Infra.Data.Mappings;
 
 namespace DevIO.Infra.Data.Context
 {
@@ -47,6 +51,24 @@ namespace DevIO.Infra.Data.Context
             modelBuilder.Configurations.Add ( new ProdutoMapping ( ) );
 
             base.OnModelCreating ( modelBuilder );
+        }
+
+        public override Task<int> SaveChangesAsync ( CancellationToken cancellationToken )
+        {
+            foreach (var entry in ChangeTracker.Entries ( ).Where ( entry => entry.Entity.GetType ( ).GetProperty ( "DataCadastro" ) != null ))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property ( "DataCadastro" ).CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property ( "DataCadastro" ).IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync ( cancellationToken );
         }
     }
 }
